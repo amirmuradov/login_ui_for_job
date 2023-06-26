54af906da9951b7e748f49b761c8c19bfd90087c
amirmuradov <sohmuradov47@gmail.com>
2023-06-26 15:04:12 +0700

    created added updated

--- lib/pages/filials_page.dart
+++ lib/pages/filials_page.dart
@@ -20,143 +20,204 @@ class _FilialsPageState extends State<FilialsPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 15,
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) {
-                              return const MainPage();
-                            },
-                          ),
-                        );
-                      },
-                      child: SvgPicture.asset('assets/arrow_back.svg'),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        right: 35,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Филлиалы",
-                          style:
-                              Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 20,
-                                  ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Stack(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Container(
+                    Padding(
                       padding: const EdgeInsets.only(
-                        top: 15,
                         left: 15,
-                        right: 170,
                       ),
-                      width: double.infinity,
-                      height: 125,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(22),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "Вы можете отправить заявки во все МКК",
-                                  textAlign: TextAlign.left,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodySmall!
-                                      .copyWith(
-                                        color: Theme.of(context)
-                                            .scaffoldBackgroundColor,
-                                        fontSize: 16,
-                                      ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          CustomBtn(
-                            onTap: () {},
-                            padding: const EdgeInsets.all(0),
-                            text: "Отправить",
-                            textStyle:
-                                Theme.of(context).textTheme.bodySmall!.copyWith(
-                                      fontSize: 14,
-                                      color: Theme.of(context).primaryColor,
-                                    ),
-                            textColor: Theme.of(context).primaryColor,
-                            accentColor:
-                                Theme.of(context).scaffoldBackgroundColor,
-                            height: 35,
-                            width: 120,
-                          ),
-                        ],
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            CupertinoPageRoute(
+                              builder: (context) {
+                                return const MainPage();
+                              },
+                            ),
+                          );
+                        },
+                        child: SvgPicture.asset('assets/arrow_back.svg'),
                       ),
                     ),
-                    Positioned(
-                      bottom: 10,
-                      right: 15,
-                      child: SvgPicture.asset(
-                        'assets/test.svg',
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          right: 35,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Филлиалы",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20,
+                                ),
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5),
-                    child: Checkbox(
-                      value: isChecked,
-                      onChanged: (newBool) {
-                        setState(() {
-                          isChecked = newBool;
-                        });
-                      },
-                      tristate: true,
-                      activeColor: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  Text(
-                    "С высоким рейтингом",
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                          top: 15,
+                          left: 15,
+                          right: 170,
                         ),
+                        width: double.infinity,
+                        height: 125,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(22),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    "Вы можете отправить заявки во все МКК",
+                                    textAlign: TextAlign.left,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodySmall!
+                                        .copyWith(
+                                          color: Theme.of(context)
+                                              .scaffoldBackgroundColor,
+                                          fontSize: 16,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 25),
+                              child: CustomBtn(
+                                onTap: () {},
+                                padding: const EdgeInsets.all(0),
+                                text: "Отправить",
+                                textStyle: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall!
+                                    .copyWith(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                textColor: Theme.of(context).primaryColor,
+                                accentColor:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                                height: 35,
+                                width: 100,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 15,
+                        child: SvgPicture.asset(
+                          'assets/test.svg',
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              CreditCart()
-            ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Checkbox(
+                        value: isChecked,
+                        onChanged: (newBool) {
+                          setState(() {
+                            isChecked = newBool;
+                          });
+                        },
+                        tristate: true,
+                        activeColor: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    Text(
+                      "С высоким рейтингом",
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 25, right: 15),
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 25),
+                          width: 120,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(66),
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset('assets/location.svg'),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "На карте",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall!
+                                    .copyWith(
+                                      color: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: CreditCart(
+                          name: "Credit cart",
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/credit_cart.dart
+++ lib/widgets/credit_cart.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CreditCart extends StatefulWidget {
   const CreditCart({
     super.key,
-    this.width = 345,
+    this.width = 343,
+    this.textStyle,
+    this.svgpicturerate,
+    required this.name,
     this.borderRadius = const BorderRadius.all(
       Radius.circular(14),
     ),
-    this.height = 205,
+    this.height = 202,
   });
   final double? height;
   final double? width;
   final BorderRadius borderRadius;
+  final String name;
+  final TextStyle? textStyle;
+  final Widget? svgpicturerate;
   @override
   State<CreditCart> createState() => _CreditCartState();
 }
@@ -23,15 +30,55 @@ class _CreditCartState extends State<CreditCart> {
       height: widget.height,
       width: widget.width,
       decoration: BoxDecoration(
-          borderRadius: widget.borderRadius,
-          color: Theme.of(context).scaffoldBackgroundColor,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 18,
-              offset: const Offset(0, 1),
-              color: Colors.grey.shade300.withOpacity(1),
-            )
-          ]),
+        borderRadius: widget.borderRadius,
+        color: Theme.of(context).scaffoldBackgroundColor,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 18,
+            offset: const Offset(0, 1),
+            color: Colors.grey.shade300.withOpacity(1),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 15,
+              top: 15,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 19,
+                  child: Text(
+                    "logo",
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          fontSize: 11,
+                          fontWeight: FontWeight.w600,
+                        ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    widget.name,
+                    style: widget.textStyle ??
+                        Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Theme.of(context).hintColor,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                            ),
+                  ),
+                ),
+                widget.svgpicturerate ?? SvgPicture.asset('assets/rating.svg')
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
